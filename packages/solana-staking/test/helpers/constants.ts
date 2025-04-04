19e03e080af14285a600e4dfdf4edd8a2acba341
Ahmed Ali <imahmedalibhatti@gmail.com>
2025-04-04 21:16:57 +0100

    test(solana-staking): add tests for deposit-to-specific-validator

--- packages/solana-staking/test/helpers/constants.ts
+++ packages/solana-staking/test/helpers/constants.ts
@@ -4,9 +4,22 @@ export const GLOBAL_NAMESPACE = "global" as const;
 
 export const RPC_URI_MAINNET = "https://api.devnet.solana.com";
 
+export const TRANSIENT_STAKE_SEED = 0;
+
+export const EPHEMERAL_STAKE_SEED = 0;
+
 /// Note: All the addresses below are for the mainnet. Also listed in `src/docs/addresses.md` ///
 
+// The stake pool program ID.
 export const STAKE_POOL_PROGRAM_ID = new PublicKey("5d33x6gSAps926kRDBuM4DwXXZq3sJrVU9tsH5ReTXpE");
+
+// PDA that holds the validator list.
+export const STAKE_POOL_VALIDATOR_LIST = new PublicKey("8M7ZbLikHJdeU6iKzZxWJ8zHy1ozX65Zk2GBVcmFVnXe");
+
+// === Stake Accounts ===
+
+// PDA that holds the ephemeral stake account.
+export const EPHEMERAL_STAKE_ACCOUNT = new PublicKey("7eJqx2aAV5ZEVBHjM3HHmQLcDqQPqcgiz2audV2yoFz9");
 
 // === Pool Accounts ===
 
@@ -20,6 +33,9 @@ export const POOL_RESERVE = new PublicKey("EDKjf7YhYZyZriHrepRZEXNjwAen9aJwqToiW
 export const POOL_MINT = new PublicKey("Eui9jWw5oqC7PwmJscHr7tDGEgZjA1zng9QW5FSciGr5");
 
 // === Authority Accounts ===
+
+// PDA that has authority to manage the staker program.
+export const STAKER_AUTHORITY = new PublicKey("3Gu6VvL43rRGUodheyitHuYGqsT7qLkuYjwjwC9V2mTU");
 
 // PDA that has authority to deposit SOL into the pool.
 export const DEPOSIT_AUTHORITY = new PublicKey("BLTSuAqaoaUUjLRVdanFXGBi2fef5pwhKN23kCMVgX2T");
@@ -52,3 +68,10 @@ export const STAKER_PROGRAM_ID = new PublicKey("6EZAJVrNQdnBJU6ULxXSDaEoK6fN7C3i
 
 // Required for account creation and rent exemption.
 export const SYSTEM_PROGRAM_ID = new PublicKey("11111111111111111111111111111111");
+
+// === Sysvars ===
+
+export const CLOCK_SYSVAR = new PublicKey("SysvarC1ock11111111111111111111111111111111");
+export const STAKE_HISTORY_SYSVAR = new PublicKey("SysvarStakeHistory1111111111111111111111111");
+export const STAKE_CONFIG_SYSVAR = new PublicKey("StakeConfig11111111111111111111111111111111");
+export const STAKE_SYSVAR = new PublicKey("Stake11111111111111111111111111111111111111");
