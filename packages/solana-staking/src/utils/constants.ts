d3a778da26b66494aa885fd930c404772d756440
Ahmed Ali <imahmedalibhatti@gmail.com>
2025-04-04 20:40:41 +0100

    feat(solana-staking): add sysvars and staker authority

--- packages/solana-staking/src/utils/constants.ts
+++ packages/solana-staking/src/utils/constants.ts
@@ -4,6 +4,10 @@ export const GLOBAL_NAMESPACE = "global" as const;
 
 export const RPC_URI_MAINNET = "https://api.mainnet-beta.solana.com";
 
+export const TRANSIENT_STAKE_SEED = 0;
+
+export const EPHEMERAL_STAKE_SEED = 0;
+
 /// Note: All the addresses below are for the mainnet. Also listed in `src/docs/addresses.md` ///
 
 // The stake pool program ID.
@@ -29,6 +33,9 @@ export const POOL_RESERVE = new PublicKey("EDKjf7YhYZyZriHrepRZEXNjwAen9aJwqToiW
 export const POOL_MINT = new PublicKey("6umRHtiuBd1PC6HQhfH9ioNsqY4ihZncZXNPiGu3d3rN");
 
 // === Authority Accounts ===
+
+// PDA that has authority to manage the staker program.
+export const STAKER_AUTHORITY = new PublicKey("3Gu6VvL43rRGUodheyitHuYGqsT7qLkuYjwjwC9V2mTU");
 
 // PDA that has authority to deposit SOL into the pool.
 export const DEPOSIT_AUTHORITY = new PublicKey("BLTSuAqaoaUUjLRVdanFXGBi2fef5pwhKN23kCMVgX2T");
@@ -61,3 +68,10 @@ export const STAKER_PROGRAM_ID = new PublicKey("6EZAJVrNQdnBJU6ULxXSDaEoK6fN7C3i
 
 // Required for account creation and rent exemption.
 export const SYSTEM_PROGRAM_ID = new PublicKey("11111111111111111111111111111111");
+
+// === Sysvars ===
+
+export const CLOCK_SYSVAR = new PublicKey("SysvarC1ock11111111111111111111111111111111");
+export const STAKE_HISTORY_SYSVAR = new PublicKey("SysvarStakeHistory1111111111111111111111111");
+export const STAKE_CONFIG_SYSVAR = new PublicKey("StakeConfig11111111111111111111111111111111");
+export const STAKE_SYSVAR = new PublicKey("Stake11111111111111111111111111111111111111");
